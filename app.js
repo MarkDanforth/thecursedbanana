0f379a3cac4b58e9069b887b524f6cc922872482
Mark Danforth <mark.danforth@atos.net>
2018-03-23 15:07:40 +0000

    Testing the add of Node

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/bleh', (req, res) => {
+    res.status(200).send({ "thing": "stuff" });
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
